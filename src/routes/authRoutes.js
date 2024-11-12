ba3907fcd1be41b4bbc88d656dc42e5e35750d5d
Antônio <amcw2@ic.ufal.br>
2024-11-12 09:11:54 -0300

    changing things

--- src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register } = require('../controllers/authController');
-
-// Rota para registro
-router.post('/register', register);
-
-module.exports = router;
